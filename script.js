820bef5be06ba326681ef0ffa20c67425e55beca
MarinB-56-Mac <150065021+MarinB-56@users.noreply.github.com>
2024-02-19 23:35:00 +0100

    Merging main and eraser

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const div = document.querySelector('div');
-const clear_button = document.querySelector('button');
+const clear_button = document.querySelector('.clear_button');
+const erase_button = document.querySelector('.erase_button');
+const color_button = document.querySelector('.color_button');
+
+//Color
+let randomColor = 0;
+let random = true; //Default value (for random color)
 
 //Creating the grid of square divs 
 function grid_creation(){
@@ -11,25 +17,37 @@ function grid_creation(){
 
             div.appendChild(new_div);
 
-            //Adding the possibility to change the color of a square when hovered
+            //Change the color of a square when hovered
             new_div.addEventListener('mouseover',() =>{
-                // console.log("coucou");
-                const randomColor = Math.floor(Math.random()*16777215).toString(16);
-                new_div.style.cssText = `background-color : #${randomColor}`;
-
-                console.log(randomColor);
+                
+                //Check wether the color shall be random or not
+                if(random){
+                    randomColor = Math.floor(Math.random()*16777215).toString(16);
+                }
+                else{
+                    randomColor = "#FFFFFF";
+                }
+                
+                new_div.style.cssText = `background-color : #${randomColor} `;
             })
 
+            //Button to clear everything on the grid (general eraser)
             clear_button.addEventListener('click', () => {
-                console.log("oui");
                 new_div.style.cssText = "background-color : white ";
             })
         }
     }
 }
 
-//Implementation of clear button
 grid_creation();
 
-//Essai pour git
+//Make the selected div white (precise eraser)
+erase_button.addEventListener('click', () =>{    
+    random = false;
+})
+
+//Randomize color of selected div 
+color_button.addEventListener('click', () => {
+    random = true;
+})
 
