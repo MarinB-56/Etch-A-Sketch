34635a31ee7ae77cacf9c9da1022c90a9e8cb95e
MarinB-56-Mac <150065021+MarinB-56@users.noreply.github.com>
2024-02-19 23:27:22 +0100

    Implementing eraser button (precise). Implementing color button (to make the div random color again)

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const div = document.querySelector('div');
 const clear_button = document.querySelector('.clear_button');
 const erase_button = document.querySelector('.erase_button');
+const color_button = document.querySelector('.color_button');
 
 //Color
 let randomColor = 0;
-randomColor=Math.floor(Math.random()*16777215).toString(16);
+let random = true; //Default value (for random color)
 
 //Creating the grid of square divs 
 function grid_creation(){
@@ -16,29 +17,37 @@ function grid_creation(){
 
             div.appendChild(new_div);
 
-            //Adding the possibility to change the color of a square when hovered
+            //Change the color of a square when hovered
             new_div.addEventListener('mouseover',() =>{
-                // console.log("coucou");
-                new_div.style.cssText = `background-color : #${randomColor}`;
-
-                // console.log(randomColor);
+                
+                //Check wether the color shall be random or not
+                if(random){
+                    randomColor = Math.floor(Math.random()*16777215).toString(16);
+                }
+                else{
+                    randomColor = "#FFFFFF";
+                }
+                
+                new_div.style.cssText = `background-color : #${randomColor} `;
             })
 
             //Button to clear everything on the grid (general eraser)
             clear_button.addEventListener('click', () => {
                 new_div.style.cssText = "background-color : white ";
             })
-
-            //Button to make the selected div white (precise eraser)
-            erase_button.addEventListener('click', () =>{    
-                console.log("erase");
-                randomColor = "#FFFFFF";
-            })
         }
     }
 }
 
-//Implementation of clear button
 grid_creation();
 
+//Make the selected div white (precise eraser)
+erase_button.addEventListener('click', () =>{    
+    random = false;
+})
+
+//Randomize color of selected div 
+color_button.addEventListener('click', () => {
+    random = true;
+})
 
